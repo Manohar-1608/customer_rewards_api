af7b9eb5eb531c48d41435baa895ad7326d35787
Manohar <manoharreddy.ka@gmail.com>
2025-07-09 23:48:16 +0530

    Added spring boot initial project setup
    Added spring boot initial project setup

---
+++ rewardscalculator/src/main/java/com/rewardpoints/rewardscalculator/controller/PointeCalculatorController.java
@@ -0,0 +1,9 @@
+package com.rewardpoints.rewardscalculator.controller;
+
+public class PointeCalculatorController {
+
+	public PointeCalculatorController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

2d0e6f9952424b70817213aa2a842c9684f1c817
Manohar <manoharreddy.ka@gmail.com>
2025-07-15 09:34:42 +0530

    aded test cases for controller and edge cases for points calculation
    aded test cases for controller and edge cases for points calculation

--- rewardscalculator/src/test/java/com/rewardpoints/rewardscalculator/controller/PointsCalculatorControllerTest.java
+++ rewardscalculator/src/test/java/com/rewardpoints/rewardscalculator/controller/PointsCalculatorControllerTest.java
@@ -1,6 +1,7 @@
 package com.rewardpoints.rewardscalculator.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.format.DateTimeFormatter;
@@ -13,7 +14,6 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.rewardpoints.rewardscalculator.rewardservice.RewardPointsService;
-import com.rewardpoints.rewardscalculator.utils.RewardConstants;
 
 @WebMvcTest(PointsCalculatorController.class)
 public class PointsCalculatorControllerTest {
@@ -24,26 +24,31 @@ public class PointsCalculatorControllerTest {
     @MockitoBean
     private RewardPointsService rewardPointsService;
 
-	public static final String MONTH_FORMAT = "yyyy-MM";
+    public static final String MONTH_FORMAT = "yyyy-MM";
 
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern(MONTH_FORMAT);
 
-	DateTimeFormatter formatter = DateTimeFormatter.ofPattern(MONTH_FORMAT);
+    @Test
+    @DisplayName("Bad request for non existing customer")
+    void shouldReturnBadRequestForBlankCustomerId() throws Exception {
+	mockMvc.perform(get("/api/reward-points/count-points").param("customerId", "  ").param("from", "2024-05-01")
+		.param("to", "2024-06-01")).andExpect(status().isBadRequest());
+    }
 
-    //class ValidationTests {
+    @Test
+    @DisplayName("Bad request for incorrect to date")
+    void shouldReturnBadRequestForInvalidDateRange() throws Exception {
+	mockMvc.perform(get("/api/reward-points/count-points").param("customerId", "C1").param("from", "2024-07-01")
+		.param("to", "2024-06-01")).andExpect(status().isBadRequest());
+    }
 
-	@Test
-	@DisplayName("")
-	void shouldReturnBadRequestForBlankCustomerId() throws Exception {
-	    mockMvc.perform(get("/api/reward-points/count-points").param("customerId", "  ").param("from", "2024-05-01")
-		    .param("to", "2024-06-01")).andExpect(status().isBadRequest());
-	}
+    @Test
+    @DisplayName("Return 202 Accepted and valid points for customer C1")
+    void shouldReturnRewardPointsForValidCustomer() throws Exception {
+	mockMvc.perform(get("/api/reward-points/count-points").param("customerId", "C1").param("from", "2025-05-01")
+		.param("to", "2025-07-31")).andExpect(status().isAccepted())
+		.andExpect(jsonPath("$.customerId").value("C1")).andExpect(jsonPath("$.customerName").value("Mohan"))
+		.andExpect(jsonPath("$.totalPoints").value(115));
+    }
 
-	@Test
-	@DisplayName("")
-	void shouldReturnBadRequestForInvalidDateRange() throws Exception {
-	    mockMvc.perform(get("/api/reward-points/count-points").param("customerId", "C1").param("from", "2024-07-01")
-		    .param("to", "2024-06-01")).andExpect(status().isBadRequest());
-	}
-
-   // }
 }
--- rewardscalculator/src/test/java/com/rewardpoints/rewardscalculator/service/RewardPointsServiceTest.java
+++ rewardscalculator/src/test/java/com/rewardpoints/rewardscalculator/service/RewardPointsServiceTest.java
@@ -1,22 +1,28 @@
 package com.rewardpoints.rewardscalculator.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
+import java.lang.reflect.Field;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.function.Function;
 
-import com.rewardpoints.rewardscalculator.exception.CustomerNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.rewardpoints.rewardscalculator.dto.RewardPointsDTO;
 import com.rewardpoints.rewardscalculator.entity.Transaction;
+import com.rewardpoints.rewardscalculator.exception.CustomerNotFoundException;
 import com.rewardpoints.rewardscalculator.repository.TransactionRepository;
 import com.rewardpoints.rewardscalculator.rewardservice.RewardPointsService;
 
@@ -28,51 +34,52 @@ public class RewardPointsServiceTest {
     @InjectMocks
     private RewardPointsService rewardService;
 
+    private Function<Double, Integer> pointsCalculator;
+
     @BeforeEach
-    public void setup() {
-        MockitoAnnotations.openMocks(this);
+    public void setup() throws Exception {
+	MockitoAnnotations.openMocks(this);
+	RewardPointsService service = new RewardPointsService(null);
+	Field field = RewardPointsService.class.getDeclaredField("pointsCalculator");
+	field.setAccessible(true);
+	this.pointsCalculator = (Function<Double, Integer>) field.get(service);
     }
 
     @Test
+    @DisplayName("Check points for different months")
     public void testCalculateRewards_MultipleMonths() {
-        List<Transaction> transactions = List.of(new Transaction("C1", "Alice", LocalDate.of(2025, 5, 10), 120),
-                new Transaction("C1", "Amit", LocalDate.of(2025, 6, 15), 75),
-                new Transaction("C1", "Amit", LocalDate.of(2025, 7, 5), 40));
+	List<Transaction> transactions = List.of(new Transaction("C1", "Amar", LocalDate.of(2025, 5, 10), 120),
+		new Transaction("C1", "Amit", LocalDate.of(2025, 6, 15), 75),
+		new Transaction("C1", "Amit", LocalDate.of(2025, 7, 5), 40));
 
-        when(repository.findByCustomerIdAndDateBetween(eq("C1"), any(), any())).thenReturn(transactions);
+	when(repository.findByCustomerIdAndDateBetween(eq("C1"), any(), any())).thenReturn(transactions);
 
-        RewardPointsDTO response = rewardService.calculateRewards("C1", LocalDate.of(2025, 5, 1),
-                LocalDate.of(2025, 7, 31));
-
-        assertEquals("C1", response.getCustomerId());
-        assertEquals("Alice", response.getCustomerName());
-        assertEquals(3, response.getMonthlyPoints().size());
-        assertEquals(115, response.getTotalPoints());
-    }
-
-    /*@Test
-    public void testCalculateRewards_NoTransactions() {
-	when(repository.findByCustomerIdAndDateBetween(eq("C1"), any(), any())).thenReturn(List.of());
-
-	RewardPointsDTO response = rewardService.calculateRewards("C1", LocalDate.of(2025, 4, 1),
+	RewardPointsDTO response = rewardService.calculateRewards("C1", LocalDate.of(2025, 5, 1),
 		LocalDate.of(2025, 7, 31));
 
 	assertEquals("C1", response.getCustomerId());
-	assertEquals("Unknown", response.getCustomerName());
-	assertTrue(response.getMonthlyPoints().isEmpty());
-	assertEquals(0, response.getTotalPoints());
-    }*/
+	assertEquals("Amar", response.getCustomerName());
+	assertEquals(3, response.getMonthlyPoints().size());
+	assertEquals(115, response.getTotalPoints());
+    }
 
     @Test
+    @DisplayName("Check for non existing customer")
     public void testCalculateRewards_NoTransactions_ShouldThrowException() {
-        when(repository.findByCustomerIdAndDateBetween(eq("C1"), any(), any())).thenReturn(List.of());
+	when(repository.findByCustomerIdAndDateBetween(eq("C1"), any(), any())).thenReturn(List.of());
 
-        CustomerNotFoundException ex = assertThrows(CustomerNotFoundException.class, () -> {
-            rewardService.calculateRewards("C1", LocalDate.of(2025, 4, 1),
-                    LocalDate.of(2025, 7, 31));
-        });
+	CustomerNotFoundException ex = assertThrows(CustomerNotFoundException.class, () -> {
+	    rewardService.calculateRewards("C1", LocalDate.of(2025, 4, 1), LocalDate.of(2025, 7, 31));
+	});
 
-        assertEquals("No transactions found for customer ID : C1", ex.getMessage());
+	assertEquals("No transactions found for customer ID : C1", ex.getMessage());
+    }
+
+    @DisplayName("Points validation for different set of inputs")
+    @ParameterizedTest(name = "Amount: {0}, Expected Points: {1}")
+    @CsvSource({ "40.0, 0", "50.0, 0", "75.0, 25", "100.0, 50", "101.0, 52", "120.0, 90" })
+    void shouldCalculatePointsCorrectly(double amount, int expectedPoints) {
+	assertEquals(expectedPoints, pointsCalculator.apply(amount));
     }
 
 }
